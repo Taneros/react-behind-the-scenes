5dc69a4a09faa150bfee364ba128b67e37976512
Taneros <fricentaurus@GMAIL.COM>
2022-08-26 08:52:40 +0300

    feat: callback gotcha

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ import DemoOutput from './Demo/Demo'
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false)
+  const [allowToggle, setAllowToggle] = useState(false)
 
   const toggleParagraphHandler = useCallback(() => {
-    setShowParagraph((prev) => !prev)
-  }, [])
+    if (allowToggle) setShowParagraph((prev) => !prev)
+  }, [allowToggle])
+
+  const toggleButton = () => {
+    setAllowToggle((prev) => !prev)
+  }
 
   console.log(`app js`)
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={toggleButton} children={'Allow toggling'} />
       <Button
         onClick={toggleParagraphHandler}
         children={'Toggle paragraph'}
         className={'toggle-btn'}
+        disabled={!allowToggle}
       />
     </div>
   )
